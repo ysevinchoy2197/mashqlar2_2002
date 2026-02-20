1c68cfad2aab8cfb809a77698dfb1dcc45323383
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-02-20 16:01:57 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+
+#1-misol
+def farq(a, b):
+    return a - b
+
+res = farq(8, 3)
+print(res)
+
+#2-misol
+def kop(a, b):
+    return a * b
+
+res = kop(3, 3)
+print(res)
+
+#3-misol
+def c(a, b):
+    return a // b
+
+res = c(4, 2)
+print(res)
+
+#4-misol
+def matn(a):
+   return (f"Siznming ismingiz: {a}")
+
+res = matn("Sevinch")
+print(res)
